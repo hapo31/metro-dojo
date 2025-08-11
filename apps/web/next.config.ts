5d1e037f5e86705d1d757a064c98c6556698e0ed
happo31 <happo31@gmail.com>
2025-08-11 13:59:05 +0000

    fix: napi-rs/canvas を外部参照として取り込む

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@napi-rs/canvas"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("@napi-rs/canvas");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
